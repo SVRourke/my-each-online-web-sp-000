7a6cb1991a7d513e6fd2cc6a52eafbb54ea9c13d
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 02:09:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+    # code here
+    i = 0
+    while i < array.count
+      if block_given?
+        yield array[i]
+        array[i]
+        i += 1
+      else
+        return array[i]
+      end
+    end
 end
+
